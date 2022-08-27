851bbeb40115b9a72bddaf1531687925a75d7f36
hjjam100 <hjjam100@gmail.com>
2022-08-27 19:09:14 +0900

    config: prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), SharedModule, UserModule],
   controllers: [],
   providers: [],
 })
